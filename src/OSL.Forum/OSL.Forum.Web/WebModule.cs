a0441dc089136d38d7d6d628aaebf5112b45f1f7
Md. Mahmudul Hasan <mhsayan.ltd@gmail.com>
2022-03-14 17:10:01 +0600

    Added Automapper for Object-Object relation mapping

--- src/OSL.Forum/OSL.Forum.Web/WebModule.cs
+++ src/OSL.Forum/OSL.Forum.Web/WebModule.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Autofac;
+using AutoMapper;
+using OSL.Forum.Web.Profiles;
 
 namespace OSL.Forum.Web
 {
@@ -10,8 +12,22 @@ namespace OSL.Forum.Web
     {
         protected override void Load(ContainerBuilder builder)
         {
+            builder.Register(context => new MapperConfiguration(cfg =>
+                {
+                    //Register Mapper Profile
+                    cfg.AddProfile<WebProfiles>();
+                }
+            )).AsSelf().InstancePerRequest();
 
+            builder.Register(c =>
+                {
+                    //This resolves a new context that can be used later.
+                    var context = c.Resolve<IComponentContext>();
+                    var config = context.Resolve<MapperConfiguration>();
+                    return config.CreateMapper(context.Resolve);
+                })
+                .As<IMapper>()
+                .InstancePerLifetimeScope();
         }
-
     }
 }
