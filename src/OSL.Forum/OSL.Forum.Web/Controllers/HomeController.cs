78426b06842fc12f18e0e9c7afc010854477e4f9
Md. Mahmudul Hasan <mhsayan.ltd@gmail.com>
2022-03-14 16:47:44 +0600

    Added Log4net logger

--- src/OSL.Forum/OSL.Forum.Web/Controllers/HomeController.cs
+++ src/OSL.Forum/OSL.Forum.Web/Controllers/HomeController.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using log4net;
 
 namespace OSL.Forum.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(HomeController));
         public ActionResult Index()
         {
+            Log.Info("Action Index has been fired.");
+
             return View();
         }
 
