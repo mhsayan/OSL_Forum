92f689f5993ee030ba63c76c8d590d657100b653
Md. Mahmudul Hasan <mhsayan.ltd@gmail.com>
2022-03-23 15:21:07 +0600

    Created Forum view page where all Topics will be listed

---
+++ src/OSL.Forum/OSL.Forum.Web/Controllers/PostController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Autofac;
+using log4net;
+using OSL.Forum.Web.Models.Post;
+
+namespace OSL.Forum.Web.Controllers
+{
+    public class PostController : Controller
+    {
+        private static readonly ILog _logger = LogManager.GetLogger(typeof(PostController));
+        private readonly ILifetimeScope _scope;
+
+        public PostController(ILifetimeScope scope)
+        {
+            _scope = scope;
+        }
+        // GET: Post
+        public ActionResult Topic(Guid id)
+        {
+            var model = _scope.Resolve<TopicViewModel>();
+            model.Resolve(_scope);
+            model.GetForum(id);
+            model.GetCategory();
+            model.LoadUserInfo();
+
+            return View(model);
+        }
+    }
+}
