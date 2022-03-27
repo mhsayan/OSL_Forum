2283c5ee6bf6368b31d9895719ea5b36901bb35c
Md. Mahmudul Hasan <mhsayan.ltd@gmail.com>
2022-03-27 15:10:09 +0600

    added Registered user to USER Role

--- src/OSL.Forum/OSL.Forum.Web/Controllers/AccountController.cs
+++ src/OSL.Forum/OSL.Forum.Web/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using OSL.Forum.Web.Models;
+using OSL.Forum.Web.Seeds;
 
 namespace OSL.Forum.Web.Controllers
 {
@@ -155,6 +156,7 @@ namespace OSL.Forum.Web.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
+                    await UserManager.AddToRoleAsync(user.Id, Roles.User);
                     await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
 
                     // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
