24bf0f6b3d7ac906a1a61b1d2fe63c6c73d503c9
Md. Mahmudul Hasan <mhsayan.ltd@gmail.com>
2022-03-27 10:15:13 +0600

    Added Profile Controller, Model & View

---
+++ src/OSL.Forum/OSL.Forum.Web/Controllers/ProfileController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Autofac;
+using log4net;
+
+namespace OSL.Forum.Web.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private static readonly ILog _logger = LogManager.GetLogger(typeof(ProfileController));
+        private readonly ILifetimeScope _scope;
+
+        public ProfileController(ILifetimeScope scope)
+        {
+            _scope = scope;
+        }
+
+        // GET: Profile
+        public ActionResult MyProfile()
+        {
+            return View();
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/OSL.Forum/OSL.Forum.Web/Models/Profile/ProfileDetailsModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OSL.Forum.Web.Models.Profile
+{
+    public class ProfileDetailsModel
+    {
+    }
+}
