4a5d3f787555258265b027ab883d0d69fb59df5f
Md. Mahmudul Hasan <mhsayan.ltd@gmail.com>
2022-03-28 17:23:07 +0600

    Updated FavoriteForum properties

--- src/OSL.Forum/OSL.Forum.Core/BusinessObjects/FavoriteForum.cs
+++ src/OSL.Forum/OSL.Forum.Core/BusinessObjects/FavoriteForum.cs
@@ -1,7 +1,15 @@
-﻿namespace OSL.Forum.Core.BusinessObjects
+﻿using System;
+using OSL.Forum.Core.Entities;
+
+namespace OSL.Forum.Core.BusinessObjects
 {
     public class FavoriteForum
     {
+        public Guid Id { get; set; }
+        public string ApplicationUserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+        public Guid ForumId { get; set; }
+        public Entities.Forum Forum { get; set; }
 
     }
 }
--- src/OSL.Forum/OSL.Forum.Core/Entities/FavoriteForum.cs
+++ src/OSL.Forum/OSL.Forum.Core/Entities/FavoriteForum.cs
@@ -1,15 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OSL.Forum.Base;
 
 namespace OSL.Forum.Core.Entities
 {
     [Table("FavoriteForums")]
-    public class FavoriteForum
+    public class FavoriteForum : IEntity<Guid>
     {
-
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        public string ApplicationUserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+        public Guid ForumId { get; set; }
+        public Forum Forum { get; set; }
     }
 }
