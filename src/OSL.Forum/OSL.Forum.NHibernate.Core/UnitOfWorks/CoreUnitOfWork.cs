25223076ec6347c191081432862b883e30a96e53
Md. Mahmudul Hasan <mhsayan.ltd@gmail.com>
2022-04-05 15:59:20 +0600

    Added NHibernate Core Library project

---
+++ src/OSL.Forum/OSL.Forum.NHibernate.Core/UnitOfWorks/CoreUnitOfWork.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OSL.Forum.Base;
+using OSL.Forum.Core.Contexts;
+using OSL.Forum.Core.Repositories;
+
+namespace OSL.Forum.Core.UnitOfWorks
+{
+    public class CoreUnitOfWork : UnitOfWork, ICoreUnitOfWork
+    {
+        public ICategoryRepository Categories { get; private set; }
+        public IForumRepository Forums { get; private set; }
+        public IPostRepository Posts { get; private set; }
+        public ITopicRepository Topics { get; private set; }
+        public IFavoriteForumRepository FavoriteForums { get; private set; }
+
+        public CoreUnitOfWork(ICoreDbContext context,
+            ICategoryRepository categories, IForumRepository forums,
+            IPostRepository posts, ITopicRepository topics,
+            IFavoriteForumRepository favoriteForums
+            ) : base((DbContext)context)
+        {
+            Categories = categories;
+            Forums = forums;
+            Posts = posts;
+            Topics = topics;
+            FavoriteForums = favoriteForums;
+        }
+    }
+}
