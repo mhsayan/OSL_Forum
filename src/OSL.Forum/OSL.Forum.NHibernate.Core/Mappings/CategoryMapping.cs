6c8398fa85c97e17bf7f6a2e65be9f8b5235b652
Md. Mahmudul Hasan <mhsayan.ltd@gmail.com>
2022-04-07 16:40:33 +0600

    Added Mapping for Category & Favorite Forum

---
+++ src/OSL.Forum/OSL.Forum.NHibernate.Core/Mappings/CategoryMapping.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentNHibernate.Mapping;
+using OSL.Forum.NHibernate.Core.Entities;
+
+
+namespace OSL.Forum.NHibernate.Core.Mappings
+{
+    public class CategoryMapping : ClassMap<Category>
+    {
+        public CategoryMapping()
+        {
+            Table("Categories");
+            Id(c => c.Id).GeneratedBy.Identity();
+            Map(c => c.Name).Length(64).Not.Nullable();
+            Map(c => c.CreationDate).Not.Nullable();
+            Map(c => c.ModificationDate).Not.Nullable();
+            HasMany(c => c.Forums).Cascade.AllDeleteOrphan();
+        }
+    }
+}
---
+++ src/OSL.Forum/OSL.Forum.NHibernate.Core/Mappings/FavoriteForumMapping.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentNHibernate.Mapping;
+using OSL.Forum.NHibernate.Core.Entities;
+
+namespace OSL.Forum.NHibernate.Core.Mappings
+{
+    public class FavoriteForumMapping : ClassMap<FavoriteForum>
+    {
+        public FavoriteForumMapping()
+        {
+            Table("FavoriteForums");
+            Id(ff => ff.Id).GeneratedBy.Identity();
+            Map(ff => ff.ApplicationUserId);
+            References(ff => ff.ApplicationUser);
+            Map(ff => ff.ForumId);
+            References(ff => ff.Forum);
+        }
+    }
+}
