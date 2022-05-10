ac89fd6192b576c50a69d036c895389ddbdf9a7b
Mahmudul Hasan Sayan <mhsayan.ltd@gmail.com>
2022-05-10 16:18:13 +0600

    Fixed namespace not found issue for category Models/CategoryModel.cs

--- src/OSL.Forum/OSL.Forum.Web/Areas/Admin/Models/CategoryModel.cs
+++ src/OSL.Forum/OSL.Forum.Web/Areas/Admin/Models/CategoryModel.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
 using OSL.Forum.Core.Utilities;
 using BO = OSL.Forum.Core.BusinessObjects;
 
@@ -15,7 +18,7 @@ namespace OSL.Forum.Web.Areas.Admin.Models
         public string Name { get; set; }
         public IList<string> Roles { get; set; }
         public IList<BO.Category> Categories { get; set; }
-        public Pager Pager { get; set; }        
+        public Pager Pager { get; set; }
         public BO.Category Category { get; set; }
         public IList<BO.Forum> Forums { get; set; }
     }
